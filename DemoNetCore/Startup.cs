1759ed0b3efb373330b357953986bb90723181dc
zebasxx <zebasxx@hotmail.com>
2018-07-31 13:32:17 -0300

    First commit

--- DemoNetCore/Startup.cs
+++ DemoNetCore/Startup.cs
@@ -27,7 +27,7 @@ namespace DemoNetCore
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello Demo!");
             });
         }
     }
